d862fb7ecc65f73f0207b49e3f0fa0991b9a13d8
Infinity233 <734750935@qq.com>
2018-07-24 12:10:55 +0800

    算法优化并添加题解

--- leetcode/11. Container With Most Water/Solution.py
+++ leetcode/11. Container With Most Water/Solution.py
@@ -8,14 +8,20 @@ class Solution:
             t = (j - i) * min(height[i], height[j])
             maxV = max(maxV, t)
             if height[i] < height[j]:
+                tt = height[i]
                 i += 1
+                while i < j and height[i] < tt:
+                    i += 1
             else:
-                j -= 1
+                tt = height[j]
+                j-=1
+                while i < j and height[j] < tt:
+                    j -= 1
 
         return maxV
 
 
 s = Solution()
-print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(s.maxArea([1,2,4,3]))
 
 # O（n^2）算法python超市 java通过
