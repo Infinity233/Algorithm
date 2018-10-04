979986549b3904f049248473fdc9768765dfdb67
Infinity233 <734750935@qq.com>
2018-10-04 22:52:53 +0800

    剑指offer

---
+++ 剑指offer/用两个栈实现队列/src/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while (!stack1.empty()) {
+            stack2.push(stack1.pop());
+        }
+        int res = stack2.pop();
+        while (!stack2.empty()) {
+            stack1.push(stack2.pop());
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+
+        s1.push(1);
+        s1.push(2);
+        s1.push(3);
+        s1.push(4);
+
+        System.out.println(s1.pop());
+        System.out.println(s1.pop());
+        System.out.println(s1.pop());
+        System.out.println(s1.pop());
+    }
+}
