4b6451894b46fb6fd36f40818dde1141b73bb9d1
yanyichen_i <y734750935@163.com>
2019-01-22 15:04:11 +0800

    简单优化

--- leetcode/322. Coin Change/src/Solution.java
+++ leetcode/322. Coin Change/src/Solution.java
@@ -2,19 +2,15 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
 
         int[] dp = new int[amount + 1];
-        dp[0] = 0;
 
         for (int i = 1; i <= amount; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int money : coins) {
-
-                if (i - money >= 0 && dp[i - money] + 1 < min && dp[i - money] != -1) {
-                    min = dp[i - money] + 1;
-                }
-            }
-            dp[i] = min == Integer.MAX_VALUE ? -1 : min;
+            dp[i] = 0x7fff_fffe;
         }
-
-        return dp[amount];
+        for (int coin : coins) {
+            for (int i = coin; i < amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == 0x7fff_fffe?-1:dp[amount];
     }
 }
