0b7f7ece3d289bfb9591438bba83dd3bc1bbeda1
734750935@qq.com <y734750935@163.com>
2019-01-23 21:03:45 +0800

    Union Path 2

--- leetcode/63. Unique Paths II/src/Solution.java
+++ leetcode/63. Unique Paths II/src/Solution.java
@@ -5,33 +5,37 @@ class Solution {
         int[][] dp = new int[h][w];
 
         for (int i = 0; i < h; i++) {
-            if(obstacleGrid[i][0] == 1)
+            if (obstacleGrid[i][0] == 1)
                 break;
-            dp[i][1] = 1;
+            dp[i][0] = 1;
         }
 
         for (int i = 0; i < w; i++) {
-            if(obstacleGrid[0][i] == 1)
+            if (obstacleGrid[0][i] == 1)
                 break;
             dp[0][i] = 1;
         }
 
         for (int i = 1; i < h; i++) {
             for (int j = 1; j < w; j++) {
-                if(obstacleGrid[i][j] == 1 || (obstacleGrid[i][j-1] == 1 && obstacleGrid[i-1][j] == 1))
+                if (obstacleGrid[i][j] == 1 || (obstacleGrid[i][j - 1] == 1 && obstacleGrid[i - 1][j] == 1))
                     continue;
 
-                if(obstacleGrid[i][j-1] == 1) {
-                    dp[i][j] = dp[i-1][j];
+                if (obstacleGrid[i][j - 1] == 1) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (obstacleGrid[i - 1][j] == 1) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
                 }
             }
         }
 
-        return -1;
+        return dp[h - 1][w - 1];
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.uniquePathsWithObstacles(new int[][]{{0,0,1},{1,1,0},{0,0,0}});
+        System.out.println(s.uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
     }
 }
